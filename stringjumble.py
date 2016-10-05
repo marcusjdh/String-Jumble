3b2bd75e0ee8d051cfdfb2c8d6182a7cd1990482
marcusjdh <marcushelble@hanovernorwichschools.org>
2016-10-05 10:14:58 -0400

    Updated from Brython Server: 10/5/2016 10:14:57 AM

--- stringjumble.py
+++ stringjumble.py
@@ -25,3 +25,17 @@ ydnah dnif yam uoy taht skcirt ro seuqinhcet wef a era erehT
 handy find may you that tricks or techniques few a are There
 erehT era a wef seuqinhcet ro skcirt taht uoy yam dnif ydnah
 """
+string= input("Please enter a string of text (the bigger the better): ")
+print('You entered "'+string+'". Now jumble it: ')
+reverse=string[::-1]
+print(reverse)
+words=string.split()
+wordreverse=words[::-1]
+for x in wordreverse:
+    print(x, end=" ")
+print()
+turn=reverse.split()
+circle=turn[::-1]
+for x in circle:
+    print(x, end=" ")
+
